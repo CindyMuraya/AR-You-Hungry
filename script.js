d015f97759c26f70c02ecec542c7d8495f27bed3
CindyMuraya <cmuraya12@gmail.com>
2024-05-19 14:21:50 +0300

    MongoDB

--- script.js
+++ script.js
@@ -173,19 +173,30 @@ function search() {
     }
 
 
-const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://cindymuraya:Nancy2k02@aryouhungry.2iezaag.mongodb.net/?retryWrites=true&w=majority&appName=ARYouHungry', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://cindymuraya:Nancy2k02@aryouhungry.2iezaag.mongodb.net/?retryWrites=true&w=majority&appName=ARYouHungry";
+    
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
     }
-};
-
-connectDB();
+    run().catch(console.dir);
+    
